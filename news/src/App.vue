7c017e9e5661d66a3d330c80a5eebdcd49f3c339
HenriqueGSena <carloshenrique3250@gmail.com>
2022-03-10 17:43:44 -0300

    primeiros acertos

--- news/src/App.vue
+++ news/src/App.vue
@@ -1,29 +1,18 @@
+/* eslint-disable vue/multi-word-component-names */
+/* eslint-disable vue/multi-word-component-names */
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <News />
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
+import './styles/global.css';
+import Vue from 'vue';
+import News from '@/components/news.vue';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+Vue.component('news', News);
+
+export default class App extends Vue {
 }
-</style>
+
+</script>
